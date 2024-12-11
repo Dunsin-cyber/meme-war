cb74265b3492e108d2ae269e0007d7c2fc77fbc4
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2024-12-11 20:23:30 +0100

    added key to link tag in card hover effect component

--- src/pages/explore/[id].tsx
+++ src/pages/explore/[id].tsx
@@ -1,7 +1,12 @@
 "use client";
 import React from "react";
-import Details_ from "@/components/Explore/Details";
+// import Details_ from "@/components/Explore/Details";
 import Head from "@/components/Head";
+import dynamic from "next/dynamic";
+
+const Details_ = dynamic(() => import("../../components/Explore/Details"), {
+  ssr: false,
+});
 
 function Details() {
   return (
