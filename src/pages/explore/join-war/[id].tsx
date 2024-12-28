e1768be39c8c37704196ee95f6a12c43d7a4d9c5
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2024-12-29 00:52:06 +0100

    fixed

--- src/pages/explore/join-war/[id].tsx
+++ src/pages/explore/join-war/[id].tsx
@@ -1,27 +1,29 @@
-// "use client";
+"use client";
 import React from "react";
 // import Details_ from "@/components/Explore/Details";
 import Head from "@/components/Head";
 import dynamic from "next/dynamic";
 import JoinWar from "@/components/JoinWar"
 
-import { GetServerSideProps } from "next";
+// import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { id } = context.params!; // Get the 'id' from the URL
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const { id } = context.params!; // Get the 'id' from the URL
 
-  return {
-    props: { id },
-  };
-};
+//   return {
+//     props: { id },
+//   };
+// };
 
 
 
-function Details({ id }: { id: string }) {
+function Details() {
   return (
     <div>
       <Head title="Join War" />
-      <JoinWar id={id} />
+      {/* <JoinWar id={id} /> */}
+            <JoinWar id={"2"} />
+
     </div>
   );
 }
