cea7962405f0efca0548e0c9e5eb8206a839d437
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2025-01-02 11:22:07 +0100

    edited pinata config file

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,5 +3,6 @@ import { PinataSDK } from "pinata-web3";
 
 export const pinata = new PinataSDK({
   pinataJwt: `${process.env.PINATA_JWT}`,
-  pinataGateway: `${process.env.NODE_ENV === "development"? "gateway.pinata.cloud": process.env.NEXT_PUBLIC_GATEWAY_URL}`,
+  pinataGateway:  "gateway.pinata.cloud"
+  // pinataGateway: `${process.env.NODE_ENV === "development"? "gateway.pinata.cloud": process.env.NEXT_PUBLIC_GATEWAY_URL}`,
 });
